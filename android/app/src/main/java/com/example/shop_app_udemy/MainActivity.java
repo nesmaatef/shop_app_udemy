01d691f96cbab0220c1e93644a4975b53ab02a25
nesma <nesmaatef96@gmail.com>
2021-07-08 15:58:12 +0200

    Initial commit

---
+++ android/app/src/main/java/com/example/shop_app_udemy/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.shop_app_udemy;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:shop_app_udemy/screens/bottom_bar.dart';
+import 'package:shop_app_udemy/screens/cart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomBarScreen(),
+    );
+  }
+}
